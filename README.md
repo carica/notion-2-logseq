b814d97e86d6df40e1fdf09759c7860c4776ce96
carica <noreply@github.com>
2022-05-30 14:33:41 +0800

    remove indent instead of replacing it with tabs

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # notion-2-logseq
+
+This is not a fully automated solution. Instead, it deals with common encounters in notion exports.
+Linux-like OS is required due to path manipulations.
--- convert.py
+++ convert.py
@@ -2,14 +2,20 @@ import os, re, configparser
 from dateutil.parser import parse
 from urllib.parse import unquote
 from functools import partial
+import filetype
+import time
+import shlex
 
 def link_replace(cur_dir, matchobj):
   link = matchobj.group(2)
-  if link.endswith('.png'):
-    file = os.path.join(cur_dir, link)
-    new_filename = os.path.split(link)[1].replace(' ', '_')
-    os.rename(file, os.path.join(logseq_assets_dir, new_filename))
-    link = '../assets/' + new_filename
+  file = os.path.join(cur_dir, link)
+  if os.path.isfile(file):
+    kind = filetype.guess(file)
+    if kind is not None and kind.mime.startswith('image'): # may add other filetypes later
+      old_filename = os.path.split(link)[1].replace(' ', '_')
+      new_filename = '{0}_{1}.{2}'.format(os.path.splitext(old_filename)[0], time.time_ns(), kind.extension)
+      os.rename(file, os.path.join(logseq_assets_dir, new_filename))
+      link = '../assets/' + new_filename
   return '[{0}]({1})'.format(matchobj.group(1), link)
 
 def notion_walk(notion_dir):
@@ -18,24 +24,28 @@ def notion_walk(notion_dir):
       csv_file = filename.path + '.csv' # there should be a csv file with corresponding name or it is not a page 
       if os.path.exists(csv_file):
         with open(csv_file, newline='\n') as csvfile:
-          if 'Name,Created,Tags,Updated' in csvfile.readline(): # iterate inner page directory
+          if 'Name,Created,Tags,' in csvfile.readline(): # iterate inner page directory
             notion_walk(filename.path) # skip databases, images directories etc
     elif filename.is_file() and filename.name.endswith('.md'):
       with open(filename.path, 'rt', encoding='utf-8') as input_file:
+        print('processing: {}'.format(filename.name))
         lines = input_file.readlines() # get notion pages content
         logseq_block_title = lines[0][2:].strip() # use first line as logseq block title
         logseq_filename = parse(lines[2][9:].strip()).strftime('%Y_%m_%d') + '.md' # use created at date as logseq filename 
         with open(os.path.join(logseq_journals_dir, logseq_filename), 'at', encoding='utf-8') as output_file: # always append to, or create logseq file
           output_file.write('- {0}\n'.format(logseq_block_title))
           headless_input = lines[4:] # skip first 4 lines
+          if lines[3].startswith('URL'): # with URL tag, there will be an additional line 
+            headless_input = lines[5:]
           for line in headless_input:
-            line_trim = line.strip()
+            #line_trim = line.replace('    ', '\t') # try to fix indent instead of strip()
+            line_trim = line.strip() # remove indent
             if len(line_trim) == 0:
               continue
             if md_link_pattern.search(line_trim):
               line_trim = unquote(line_trim) # unquote urlencoded characters
               line_trim = md_link_pattern.sub(partial(link_replace, notion_dir), line_trim) # replace link text & move png to assets
-            if line_trim[0] == '-':
+            if line_trim[0] == '-': # fix indent if this line is a unordered list
               output_file.write('\t{0}\n'.format(line_trim))
             else: 
               output_file.write('\t- {0}\n'.format(line_trim))
